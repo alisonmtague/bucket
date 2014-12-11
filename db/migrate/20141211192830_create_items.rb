4c0bde7cf17f18e28f77552b45b91574002787e4
Alison Tague <alisonmtague@gmail.com>
2014-12-11 15:54:13 -0500

    added some missing lines of code from pulled files

--- db/migrate/20141211192830_create_items.rb
+++ db/migrate/20141211192830_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration
   def change
   	create_table :items do |t|
-  		t.string :title
-  		t.text :description
-
+  		t.string :title, null: false
+  		t.text :desc, default: ""
+  		t.boolean :is_done, default: false
   		t.timestamps
   	end
   end
